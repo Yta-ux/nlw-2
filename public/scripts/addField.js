e31189f8050d2fdd1ca39eb7d230d6da1404c5af
Ítalo Gustavo <itamelo555@gmail.com>
2020-08-08 04:25:35 -0300

    Criando back-end
    Criação de  servidor, rotas e estruturas de formulários.

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -1,18 +1,18 @@
 document.querySelector('#add-time') // Pegando o botão
-.addEventListener('click', cloneField); // Adicionando evento
+.addEventListener('click', cloneField) // Adicionando evento
 
 // Função para adicionar um novo campo
 function cloneField() {  
-    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true); // Duplicando elemento
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true) // Duplicando elemento
 
-    const fields = newFieldContainer.querySelectorAll('input');  // Procurando os inputs
+    const fields = newFieldContainer.querySelectorAll('input')  // Procurando os inputs
     
     // Limpando inputs
     fields.forEach(function(field) {
         // Pegando o clone e o limpando
         field.value = ""
-    });
+    })
 
     document.querySelector('#schedule-items').appendChild(newFieldContainer);  // Adicionando a variável na página
-};
+}
 
